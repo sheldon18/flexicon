95aa4ea80305cbd77a1de2d50c1a3d0249a259e9
Ubuntu <ubuntu@ip-172-31-92-149.ec2.internal>
2020-03-30 08:06:47 +0000

    added template inheritance, added materialize script tags, added accordion

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
+#flask app
 app = Flask(__name__)
+
+#mongo variables
+app.config["MONDO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+#PyMongo Instance
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Flask Test 2'
+@app.route('/home_page')
+def home_page():
+    return render_template("index.html", words=mongo.db.words.find())
+    
+    
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
